983446eebd7d4019771d53b506d6b55d22e40b31
unknown <kangyu009@ljstaff.com>
2023-11-13 20:08:24 +0800

    bugfix

--- train/scripts/run_rm_multi-machine.sh
+++ train/scripts/run_rm_multi-machine.sh
@@ -9,7 +9,7 @@ RANK=$3
 MASTER_ADDR=$4
 MASTER_PORT=$5
 
-KE_ROOT_PATH = "$BELLE_PATH/saved_models"
+KE_ROOT_PATH = "/nfs/a100-80G-17/kangyu/consistency_hallucinations/BELLE/saved_models"
 ALI_ROOT_PATH = "/nfs/10.232.64.52/nvme3/kangyu/saved_models"
 
 model_name_or_path="/nfs/a100-80G-18/xunxianghui/gitrepositories/Chathome-14B-roleSFT/chatmodel-ft-roleinjection_qwen"
