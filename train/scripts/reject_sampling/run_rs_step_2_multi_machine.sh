53df68f2e2972d26b957451c7c90faa01007c6ca
unknown <kangyu009@ljstaff.com>
2023-11-15 14:27:26 +0800

    rewarding

--- train/scripts/reject_sampling/run_rs_step_2_multi_machine.sh
+++ train/scripts/reject_sampling/run_rs_step_2_multi_machine.sh
@@ -9,13 +9,13 @@ RANK=$3
 MASTER_ADDR=$4
 MASTER_PORT=$5
 
-model_name_or_path="/nfs/10.232.64.52/nvme4/xhsun/saved_models/qwen_chat_estate_epoch1/"
-reward_model_name_or_path="/nfs/10.232.64.52/nvme3/kangyu/saved_models/qwen_chat_estate_epoch1_ultrafeedback_womargin/checkpoint-1986"
+model_name_or_path="..."
+reward_model_name_or_path="$BELLE_PATH/saved_models/chatmodel-ft-roleinjection_qwen_translated_ultra_shp_no-margin_multi-machine/checkpoint-2700"
 
-output_dir="/nfs/10.232.64.52/nvme3/kangyu/saved_models/qwen_chat_estate_epoch1_RM_ultrafeedback_womargin_RS_it_data_each_1w_iter-0"
+output_dir="$BELLE_PATH/saved_models/chatmodel-ft-roleinjection_qwen_RM_ultra_shp_no-margin_RS_jiazhuang_1w_20231113"
 mkdir -p ${output_dir}
 
-instruction_file="$BELLE_PATH/data/it_data_sample_1w_each.json"
+instruction_file="..."
 
 cache_dir=hf_cache_dir
 mkdir -p ${cache_dir}
