6d614d987dae5381e88489ba02781a0a9ab9bc55
unknown <kangyu009@ljstaff.com>
2023-12-13 17:02:12 +0800

    try ppo

--- train/scripts/run_ppo.sh
+++ train/scripts/run_ppo.sh
@@ -1,7 +1,7 @@
 export CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7"
 gpus=8
 
-BELLE_PATH=".../BELLE"
+BELLE_PATH="/nfs/a100-80G-17/kangyu/consistency_hallucinations/BELLE"
 export PYTHONPATH=$BELLE_PATH/train
 
 export WANDB_PROJECT=...
@@ -9,20 +9,20 @@ export WANDB_RUN_ID=...
 export WANDB_RESUME=allow
 
 model_name_or_path=...
-output_dir="$BELLE_PATH/saved_models/$WANDB_PROJECT/$WANDB_RUN_ID"
+output_dir="$BELLE_PATH/saved_models/try_ppo"
 mkdir -p ${output_dir}
 
-train_file=$BELLE_PATH/data/xxx.jsonl
+train_file=/nfs/a100-80G-15/xytian/myProjects/AI_NLP_GM/data/ppo_test/ppo_test.train.json
 cache_dir=hf_cache_dir
 mkdir -p ${cache_dir}
 
 accelerate launch \
-    --config_file configs/accelerate_config_ppo.yaml \
+    --config_file $BELLE_PATH/train/configs/accelerate_config_ppo.yaml \
     --num_processes $gpus \
     --main_process_port 29600 \
-    "src/entry_point/ppo_train.py" \
-    --model_name $model_name_or_path \
-    --reward_model_name $model_name_or_path \
+    "$BELLE_PATH/train/src/entry_point/ppo_train.py" \
+    --model_name "/nfs/a100-80G-15/xytian/myProjects/AI_NLP_GM/pretrained_models/Llama-2-7b-chat-hf" \
+    --reward_model_name "/nfs/a100-80G-15/xytian/myProjects/AI_NLP_GM/pretrained_models/Llama-2-7b-chat-hf" \
     --train_data $train_file \
     --cache_dir $cache_dir \
     --adafactor False \
