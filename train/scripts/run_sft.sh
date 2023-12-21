036a0c4ad82d2112f697a78e6c55a08a7c317021
unknown <kangyu009@ljstaff.com>
2023-12-21 12:12:19 +0800

    train cot-lora 13b

--- train/scripts/run_sft.sh
+++ train/scripts/run_sft.sh
@@ -7,12 +7,12 @@ export ABS_PATH="/nfs/a100-80G-17/kangyu/consistency_hallucinations"
 export BELLE_PATH="$ABS_PATH/BELLE"
 export PYTHONPATH="$BELLE_PATH/train"
 
-model_name_or_path=$BELLE_PATH/saved_models/implicit_cot_lora_merged_llama2-7b
+model_name_or_path=/nfs/a100-80G-15/xytian/myProjects/AI_NLP_GM/pretrained_models/Llama-2-13b-chat-hf
 
-train_file=$ABS_PATH/trytry/implicit_cot/data/gsm8k/train_answer_train.json
-validation_file=$ABS_PATH/trytry/implicit_cot/data/gsm8k/train_answer_test.json
+train_file=$ABS_PATH/trytry/implicit_cot/data/gsm8k/train_cot_train.json
+validation_file=$ABS_PATH/trytry/implicit_cot/data/gsm8k/train_cot_test.json
 
-output_dir="$BELLE_PATH/saved_models/implicit_cot_merged-lora_llama2-7b_1e-5"
+output_dir="$BELLE_PATH/saved_models/implicit-cot_cot-lora_llama2-13b_2e-4"
 mkdir -p ${output_dir}
 
 cache_dir=hf_cache_dir
@@ -87,7 +87,7 @@ torchrun --nproc_per_node 8 $BELLE_PATH/train/src/entry_point/sft_train.py \
     --ddp_timeout 36000 \
     --model_name_or_path ${model_name_or_path} \
     --llama True \
-    --use_lora False \
+    --use_lora True \
     --deepspeed $BELLE_PATH/train/configs/deepspeed_config_stage3.json \
     --lora_config $BELLE_PATH/train/configs/lora_config_llama.json \
     --train_file ${train_file} \
@@ -95,11 +95,11 @@ torchrun --nproc_per_node 8 $BELLE_PATH/train/src/entry_point/sft_train.py \
     --per_device_train_batch_size 2 \
     --per_device_eval_batch_size 2 \
     --gradient_accumulation_steps 8 \
-    --num_train_epochs 2 \
+    --num_train_epochs 8 \
     --model_max_length ${cutoff_len} \
     --save_strategy "steps" \
     --save_total_limit 100 \
-    --learning_rate 8e-6 \
+    --learning_rate 2e-4 \
     --weight_decay 0.00001 \
     --warmup_ratio 0.01 \
     --lr_scheduler_type "cosine" \
