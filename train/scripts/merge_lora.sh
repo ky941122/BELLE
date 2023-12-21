96fcdf40687e6aa07ea76cbdf927aef906386006
unknown <kangyu009@ljstaff.com>
2023-12-21 10:14:54 +0800

    bugfix

--- train/scripts/merge_lora.sh
+++ train/scripts/merge_lora.sh
@@ -7,7 +7,7 @@ model_name_or_path=$BELLE_PATH/saved_models/Llama-2-7b-chat-hf
 lora_path=$BELLE_PATH/saved_models/implicit_cot_lora_llama2-7b
 output_path=$BELLE_PATH/saved_models/implicit_cot_lora_merged_llama2-7b
 
-CUDA_VISIBLE_DEVICES=0 python src/merge_llama_with_lora.py \
+CUDA_VISIBLE_DEVICES=0 python $BELLE_PATH/train/src/merge_llama_with_lora.py \
     --model_name_or_path ${model_name_or_path} \
     --output_path ${output_path} \
     --lora_path ${lora_path} \
