e26de628b1fa780758196cf8f7ecb4e18eb1565f
unknown <kangyu009@ljstaff.com>
2023-12-25 16:23:48 +0800

    infer cot token model

--- train/src/llama2_inference.py
+++ train/src/llama2_inference.py
@@ -1,18 +1,29 @@
 import os
 os.environ['CUDA_VISIBLE_DEVICES'] = "2"
 import json
+import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from tqdm import tqdm
 
-model_dir = "/nfs/10.232.64.3/nvme3/kangyu/saved_models/final_answer_model/checkpoint-112"
-tokenizer = AutoTokenizer.from_pretrained("/nfs/a100-80G-17/kangyu/consistency_hallucinations/BELLE/saved_models/implicit-cot_cot-lora_llama2-13b_2e-4/checkpoint-168/merged/", trust_remote_code=True)
+model_dir = "/nfs/a100-80G-15/kangyu/saved_models/Llama-2-13b-257-cot-tokens/checkpoint-168/"
+tokenizer = AutoTokenizer.from_pretrained(model_dir, trust_remote_code=True)
 tokenizer.add_special_tokens({'bos_token': '<s>', 'eos_token': '</s>', 'unk_token': '<unk>', 'pad_token': '<unk>'})
 model = AutoModelForCausalLM.from_pretrained(model_dir, device_map="cuda", trust_remote_code=True).eval()
 
-prompt = "Human: \n" + "{}" + "\n\nAssistant: \n"
+belle_prompt = "Human: \n" + "{}" + "\n\nAssistant: \n"
 
-def model_call(text, eos_token_ids):
-    inputs = tokenizer([text], padding=True, return_tensors='pt').to("cuda")
+START_COT_ID_0 = 32000
+END_COT_ID = 32256
+COT_LEN = 50
+instruction = "[INST] " + "{}" + " [/INST]"
+
+def model_call(eos_token_ids, inputs=None, text=None):
+
+    if inputs is None:
+        inputs = tokenizer([text], padding=True, return_tensors='pt').to("cuda")
+    else:
+        inputs = {"input_ids": inputs}
+
     outputs = model.generate(**inputs, eos_token_id=eos_token_ids, do_sample=False)
     output = outputs[0]
     output = output[len(inputs['input_ids'][0]):]
@@ -26,15 +37,27 @@ with open("/nfs/a100-80G-17/kangyu/consistency_hallucinations/trytry/implicit_co
 output = []
 for one in tqdm(data):
     question = one['question']
-    text = prompt.format(question)
-    res = model_call(text, [tokenizer.eos_token_id])
+    text = instruction.format(question)
+    instruct_id = tokenizer.encode(text, add_special_tokens=False)
+    input_cot_id = []
+    for i in range(COT_LEN):
+        input_cot_id.append(START_COT_ID_0 + i)
+    input_cot_id = input_cot_id + [END_COT_ID]
+
+    input_id = instruct_id + input_cot_id
+    input_id = [tokenizer.bos_token_id] + input_id
+    input_ids = torch.tensor([input_id]).to("cuda")
+
+    res = model_call(eos_token_ids=[tokenizer.eos_token_id], inputs=input_ids)
 
     new_one = dict(one)
     new_one['response'] = res
     output.append(new_one)
 
 dst_dir = "/nfs/a100-80G-17/kangyu/consistency_hallucinations/BELLE/results"
-with open(os.path.join(dst_dir, "lora-merged_final_answer_model-checkpoint-112.json"), 'w') as f:
+with open(os.path.join(dst_dir, "257-cot-tokens_first-round_COT_LEN-{}.json".format(COT_LEN)), 'w') as f:
     json.dump(output, f)
 
 print("Done!")
+
+
