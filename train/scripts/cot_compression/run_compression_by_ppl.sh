fce7acf5b7d8b9f1939773fdf5ceade5a9bb9793
unknown <kangyu009@ljstaff.com>
2023-12-27 17:33:19 +0800

    multi mchine

--- train/scripts/cot_compression/run_compression_by_ppl.sh
+++ train/scripts/cot_compression/run_compression_by_ppl.sh
@@ -1,8 +1,13 @@
 export CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7"
-gpus=8
 
 BELLE_PATH="/nfs/a100-80G-17/kangyu/consistency_hallucinations/BELLE"
 export PYTHONPATH="$BELLE_PATH/train"
+
+WORKER_GPU=$1
+WORKER_NUM=$2
+RANK=$3
+MASTER_ADDR=$4
+MASTER_PORT=$5
 
 model_name_or_path="/nfs/a100-80G-15/xytian/myProjects/AI_NLP_GM/pretrained_models/Llama-2-13b-chat-hf"
 
@@ -15,13 +20,17 @@ cache_dir=hf_cache_dir
 mkdir -p ${cache_dir}
 
 accelerate launch \
-    --config_file $BELLE_PATH/train/configs/accelerate_config_rs.yaml \
-    --num_processes $gpus \
+    --config_file $BELLE_PATH/train/configs/accelerate_config_rs_multi_machine.yaml \
+    --num_processes $WORKER_GPU \
+    --num_machines $WORKER_NUM \
+    --machine_rank $RANK \
+    --main_process_ip $MASTER_ADDR \
+    --main_process_port $MASTER_PORT \
     "$BELLE_PATH/train/src/entry_point/cot_compression/compression_by_ppl.py" \
     --output_dir $output_dir \
     --model_name $model_name_or_path \
     --train_data $train_file \
     --cache_dir $cache_dir \
     --debug True \
-    --start_n_sub 3 \
+    --start_n_sub 0 \
     --inference_batch_size_per_device 10
