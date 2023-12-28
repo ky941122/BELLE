e6e9bd30299118fde7428e595d5651ee74cc2037
unknown <kangyu009@ljstaff.com>
2023-12-28 16:33:43 +0800

    split 3

--- train/scripts/run_llama2_for_gsm8k.sh
+++ train/scripts/run_llama2_for_gsm8k.sh
@@ -1,16 +1,16 @@
-export CUDA_VISIBLE_DEVICES="1,2,3,4,5,6,7"
-gpus=7
+export CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7"
+gpus=8
 
 BELLE_PATH="/nfs/a100-80G-17/kangyu/consistency_hallucinations/BELLE"
 export PYTHONPATH="$BELLE_PATH/train"
 
 model_name_or_path="/nfs/172.17.3.40/nvme1/a100-test-1node-torchimg-idc/bella/xhsun/llama2-models/Llama-2-13b-chat-hf"
 
-output_dir="/nfs/a100-80G-14/kangyu/saved_models/Llama-2-13b_GSM8K_2"
+output_dir="/nfs/a100-80G-14/kangyu/saved_models/Llama-2-13b_GSM8K_3"
 mkdir -p ${output_dir}
 
-train_file=/nfs/a100-80G-17/kangyu/consistency_hallucinations/trytry/cot_compression/data/gsm8k_train_split_2.json
-test_file=/nfs/a100-80G-17/kangyu/consistency_hallucinations/trytry/cot_compression/data/gsm8k_test_split_2.json
+train_file=/nfs/a100-80G-17/kangyu/consistency_hallucinations/trytry/cot_compression/data/gsm8k_train_split_3.json
+test_file=/nfs/a100-80G-17/kangyu/consistency_hallucinations/trytry/cot_compression/data/gsm8k_test_split_3.json
 
 cache_dir=hf_cache_dir
 mkdir -p ${cache_dir}
