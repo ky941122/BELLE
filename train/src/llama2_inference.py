6a4a21cd366d439224d541021d404ee53d437483
unknown <kangyu009@ljstaff.com>
2024-01-02 15:52:24 +0800

    infer

--- train/src/llama2_inference.py
+++ train/src/llama2_inference.py
@@ -1,11 +1,11 @@
 import os
-os.environ['CUDA_VISIBLE_DEVICES'] = "7"
+os.environ['CUDA_VISIBLE_DEVICES'] = "6"
 import json
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from tqdm import tqdm
 
-model_dir = "/nfs/a100-80G-14/kangyu/saved_models/Llama-2-13b_GSM8K_full-train-full-gen_consistency"
+model_dir = "/nfs/a100-80G-14/kangyu/saved_models/Llama-2-13b_GSM8K"
 
 tokenizer = AutoTokenizer.from_pretrained(model_dir, trust_remote_code=True)
 tokenizer.add_special_tokens({'bos_token': '<s>', 'eos_token': '</s>', 'unk_token': '<unk>', 'pad_token': '<unk>'})
@@ -48,7 +48,7 @@ for one in tqdm(data):
     output.append(new_one)
 
 dst_dir = "/nfs/a100-80G-17/kangyu/consistency_hallucinations/BELLE/results"
-with open(os.path.join(dst_dir, "Llama-2-13b_GSM8K_full-train-full-gen_consistency.json"), 'w') as f:
+with open(os.path.join(dst_dir, "Llama-2-13b_GSM8K_full-train.json"), 'w') as f:
     json.dump(output, f)
 
 print("Done!")
